a8e9e9021360e31982bef31ed7ca5bab92c09605
Mathswesh <agniveshpatel1910@gmail.com>
2025-05-27 12:48:55 +0530

    This is until springboot , docker , microservices , grpc , kafka , security.

---
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.LoginRequestDTO;
+import com.pm.authservice.dto.LoginResponseDTO;
+import com.pm.authservice.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "Generate token for user login.")
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+
+        Optional<String> tokenOptimal = authService.authenticate(loginRequestDTO);
+        if (tokenOptimal.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        String token = tokenOptimal.get();
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @Operation(summary = "Validate Token")
+    @GetMapping("/validate")
+    public ResponseEntity<Void> validateToken(@RequestHeader("Authorization") String authHeader) {
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return authService.validateToken(authHeader.substring(7))
+                ? ResponseEntity.ok().build()
+                : ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+}
--- patient-service/src/main/java/com/patientmanagement/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/patientmanagement/patient_service/service/PatientService.java
@@ -5,6 +5,7 @@ import com.patientmanagement.patient_service.dto.PatientResponseDTO;
 import com.patientmanagement.patient_service.exception.EmailAlreadyExistsException;
 import com.patientmanagement.patient_service.exception.PatientNotFoundException;
 import com.patientmanagement.patient_service.grpc.BillingServiceGrpcClient;
+import com.patientmanagement.patient_service.kafka.KafkaProducer;
 import com.patientmanagement.patient_service.mapper.PatientMapper;
 import com.patientmanagement.patient_service.model.Patient;
 import com.patientmanagement.patient_service.repository.PatientRepository;
@@ -19,10 +20,12 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
     private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final KafkaProducer kafkaProducer;
 
-    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient){
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient, KafkaProducer kafkaProducer){
         this.patientRepository = patientRepository;
         this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
     public List<PatientResponseDTO> getPatients() {
@@ -40,6 +43,9 @@ public class PatientService {
         Patient newPatient = patientRepository.save(
                 PatientMapper.toModel(patientRequestDTO));
         billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(), newPatient.getName(), newPatient.getEmail());
+
+        kafkaProducer.sendEvent(newPatient);
+
         return PatientMapper.toDTO(newPatient);
     }
 
